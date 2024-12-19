237593eee1cafef831fb0c61ed1c229518456d00
eray <127036916+erayyunall@users.noreply.github.com>
2024-12-19 23:34:46 +0300

    Add files via upload

---
+++ alignment/alignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alignment
+//
+//  Created by ERAY ÜNAL on 18.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
